732937bc47d01f1488c858bfc21258a14441e642
Dehemi Weerakkody <dehemiweerakkodi2@gmail.com>
2024-12-01 16:51:22 +0530

    Add amount to the prompt

--- cmb-backend/src/main/java/com/dehemi/combank/dao/AITransaction.java
+++ cmb-backend/src/main/java/com/dehemi/combank/dao/AITransaction.java
@@ -2,17 +2,21 @@ package com.dehemi.combank.dao;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class AITransaction {
     String hash;
     String description;
     String defaultTag;
+    BigDecimal amount;
 
     public static AITransaction from(Transaction transaction) {
         AITransaction aiTransaction = new AITransaction();
         aiTransaction.hash = transaction.getHash();
         aiTransaction.description = transaction.getDescription();
         aiTransaction.defaultTag = transaction.getDefaultTag();
+        aiTransaction.amount = transaction.getDebit();
         return aiTransaction;
     }
 }
